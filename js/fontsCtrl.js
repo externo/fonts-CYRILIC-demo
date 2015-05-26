0d1a4bb51c3609749892c94688fcb81cbbffda48
Iliya Iliev <iliya.vasilev.iliev@gmail.com>
2015-05-26 17:25:59 +0300

    put angular logic

---
+++ js/fontsCtrl.js
@@ -0,0 +1,101 @@
+app.controller('fontsCtrl', ['$scope', function ($scope) {
+    $scope.fonts = [
+        {
+            name: 'Arkhip',
+            src: 'fonts/Arkhip_font.ttf'
+        },
+        {
+            name: 'Arvo',
+            src: 'fonts/Arvo-Regular.ttf'
+        },
+        {
+            name: 'Atlantid',
+            src: 'fonts/ATLANTID.ttf'
+        },
+        {
+            name: 'Azbuka2',
+            src: 'fonts/Azbuka02.ttf'
+        },
+        {
+            name: 'Azbuka3',
+            src: 'fonts/Azbuka03_D.ttf'
+        },
+        {
+            name: 'Blago',
+            src: 'fonts/BLAGO/BLAGO.ttf'
+        },
+        {
+            name: 'Cirpnova',
+            src: 'fonts/CIRPNOVA_D.ttf'
+        },
+        {
+            name: 'Dilyan',
+            src: 'fonts/dilyan.ttf'
+        },
+        {
+            name: 'Goran',
+            src: 'fonts/GORAN.ttf'
+        },
+        {
+            name: 'GoranC',
+            src: 'fonts/GORAN_C.ttf'
+        },
+        {
+            name: 'Jagodina',
+            src: 'fonts/JAGODINA.ttf'
+        },
+        {
+            name: 'JgdPraz',
+            src: 'fonts/JGD_PRAZ.ttf'
+        },
+        {
+            name: 'JgdPrko',
+            src: 'fonts/JGD_PRKO.ttf'
+        },
+        {
+            name: 'Mirosln',
+            src: 'fonts/MIROSLN.ttf'
+        },
+        {
+            name: 'Naopak',
+            src: 'fonts/NAOPAK.ttf'
+        },
+        {
+            name: 'Nikola',
+            src: 'fonts/NIKOLA.ttf'
+        },
+        {
+            name: 'NikolaL',
+            src: 'fonts/NIKOLA_L.ttf'
+        },
+        {
+            name: 'Oksana',
+            src: 'fonts/OksanaUADemo.ttf'
+        },
+        {
+            name: 'Pravosla',
+            src: 'fonts/pravosla.ttf'
+        },
+        {
+            name: 'Spomenar2',
+            src: 'fonts/SPOMENAR2.ttf'
+        },
+        {
+            name: 'Spomenar6',
+            src: 'fonts/SPOMENAR6.ttf'
+        },
+        {
+            name: 'SrpskaKniga',
+            src: 'fonts/SRPSKA_KNIGA/SKNIGA13.ttf'
+        },
+        {
+            name: 'Ukije',
+            src: 'fonts/UKIJJe.ttf'
+        },
+        {
+            name: 'Vuk34',
+            src: 'fonts/VUK-34.ttf'
+        }
+
+    ]
+}]);
